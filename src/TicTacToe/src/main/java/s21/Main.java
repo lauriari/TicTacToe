50d8fe2a72b00f079cf77a83a98216ea65e4ef1c
lauriari <rogovdenis1@gmail.com>
2025-04-03 18:08:14 +0700

    1st commit

---
+++ src/TicTacToe/src/main/java/s21/Main.java
@@ -0,0 +1,16 @@
+package s21;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello and welcome!");
+
+
+        for (int i = 1; i <= 5; i++) {
+
+
+            System.out.println("i = " + i);
+        }
+    }
+}
