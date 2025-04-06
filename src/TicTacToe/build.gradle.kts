70c0e5c9dedb24550f083bdfe0b102c83a37d64e
lauriari <rogovdenis1@gmail.com>
2025-04-06 16:45:04 +0700

    new...

--- src/TicTacToe/build.gradle.kts
+++ src/TicTacToe/build.gradle.kts
@@ -1,19 +1,28 @@
 plugins {
-    id("java")
+	java
+	id("org.springframework.boot") version "3.4.4"
+	id("io.spring.dependency-management") version "1.1.7"
 }
 
-group = "s21"
-version = "1.0-SNAPSHOT"
+group = "TicTacToe"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(18)
+	}
+}
 
 repositories {
-    mavenCentral()
+	mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
-tasks.test {
-    useJUnitPlatform()
+tasks.withType<Test> {
+	useJUnitPlatform()
 }
